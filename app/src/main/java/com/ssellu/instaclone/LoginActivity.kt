f2dfbbcc2d9f7326c9191e8f23a402b6a06d8259
Ssellu <ssellu.lee@gmail.com>
2021-02-26 04:00:48 +0900

    Add SignIn with Google

--- app/src/main/java/com/ssellu/instaclone/LoginActivity.kt
+++ app/src/main/java/com/ssellu/instaclone/LoginActivity.kt
@@ -1,16 +1,23 @@
 package com.ssellu.instaclone
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ProgressBar
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.auth.FirebaseAuth
-import java.lang.IllegalArgumentException
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 
 class LoginActivity : AppCompatActivity() {
 
@@ -21,9 +28,9 @@ class LoginActivity : AppCompatActivity() {
     lateinit var mFacebookButton: FloatingActionButton
     lateinit var mProgressBar: ProgressBar
 
-    var auth:FirebaseAuth? = null
 
-
+    var auth: FirebaseAuth? = null
+    var googleSignInClient: GoogleSignInClient? = null /// TODO 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,29 +45,89 @@ class LoginActivity : AppCompatActivity() {
 
 
         auth = FirebaseAuth.getInstance()
+        // TODO 2
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        // TODO 3
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
 
-        mLoginButton.setOnClickListener{
+
+
+        mLoginButton.setOnClickListener {
             try {
-            signInAndSignUp(mEmailEditText.text.toString(), mPasswordEditText.text.toString())
-            } catch (e : IllegalArgumentException){
-                Toast.makeText(this, "Your email and password cannot be empty.", Toast.LENGTH_SHORT).show()
+                signInAndSignUp(mEmailEditText.text.toString(), mPasswordEditText.text.toString())
+            } catch (e: IllegalArgumentException) {
+                Toast.makeText(this, "Your email and password cannot be empty.", Toast.LENGTH_SHORT)
+                    .show()
                 mProgressBar.visibility = View.GONE
             }
 
         }
 
+
+        // TODO 5
+        mGoogleButton.setOnClickListener {
+            // step 1
+            googleLogin()
+        }
+
     }
 
-    private fun signInAndSignUp(email:String, password:String) {
+    // TODO 4
+    private fun googleLogin() {
+        val signInIntent = googleSignInClient?.signInIntent
+        startActivityForResult(signInIntent, GOOGLE_LOGIN_CODE)
+    }
+
+    // TODO 6
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == GOOGLE_LOGIN_CODE) {
+            val result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)!!
+            if (result.isSuccess) {
+                val account = result.signInAccount
+
+                // Step 2
+                firebaseAuthWithGoogle(account)
+            }
+        }
+    }
+
+    // TODO 7
+    private fun firebaseAuthWithGoogle(account: GoogleSignInAccount?) {
+
+        val credential = GoogleAuthProvider.getCredential(account?.idToken!!, null)
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener {
+                try {
+                    when {
+                        it.isSuccessful -> startMainActivity(it.result?.user)
+                        else -> throw Exception(it.exception!!.message)
+                    }
+
+                } catch (e: Exception) {
+                    Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+                }
+                mProgressBar.visibility = View.GONE
+
+            }
+
+    }
+
+    private fun signInAndSignUp(email: String, password: String) {
         mProgressBar.visibility = View.VISIBLE
-        auth?.createUserWithEmailAndPassword(email, password)?.addOnCompleteListener{
+        auth?.createUserWithEmailAndPassword(email, password)?.addOnCompleteListener {
             try {
+
                 when {
-                    it.isSuccessful -> startMainActivity()
+                    it.isSuccessful -> startMainActivity(it.result?.user)
                     it.exception!!.message!!.isNotEmpty() -> signIn(email, password)
                     else -> throw Exception(it.exception!!.message)
                 }
-            } catch(e:Exception) {
+            } catch (e: Exception) {
                 Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
             }
             mProgressBar.visibility = View.GONE
@@ -69,14 +136,14 @@ class LoginActivity : AppCompatActivity() {
 
     private fun signIn(email: String, password: String) {
         mProgressBar.visibility = View.VISIBLE
-        auth?.signInWithEmailAndPassword(email, password)?.addOnCompleteListener{
+        auth?.signInWithEmailAndPassword(email, password)?.addOnCompleteListener {
 
             try {
                 when {
-                    it.isSuccessful -> startMainActivity()
+                    it.isSuccessful -> startMainActivity(it.result?.user)
                     else -> throw Exception(it.exception!!.message)
                 }
-            } catch(e:Exception) {
+            } catch (e: Exception) {
                 Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
             }
             mProgressBar.visibility = View.GONE
@@ -84,13 +151,17 @@ class LoginActivity : AppCompatActivity() {
 
     }
 
-    private fun startMainActivity() {
+    private fun startMainActivity(user: FirebaseUser?) {
+        Log.d("aa", "user : " + user.toString()!!)
+        if (user == null) {
+            return
+        }
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
         finish()
     }
 
-
-
-
+    companion object {
+        private const val GOOGLE_LOGIN_CODE = 9001 /// TODO 1
+    }
 }
