8f65f2b945145bbe7693ab27975c674d0c847d6a
Ssellu <ssellu.lee@gmail.com>
2021-02-26 02:43:12 +0900

    Add ProgressBar to LoginActivity

--- app/src/main/java/com/ssellu/instaclone/LoginActivity.kt
+++ app/src/main/java/com/ssellu/instaclone/LoginActivity.kt
@@ -3,8 +3,10 @@ package com.ssellu.instaclone
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ProgressBar
 import android.widget.Toast
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.auth.FirebaseAuth
@@ -17,6 +19,7 @@ class LoginActivity : AppCompatActivity() {
     lateinit var mLoginButton: Button
     lateinit var mGoogleButton: FloatingActionButton
     lateinit var mFacebookButton: FloatingActionButton
+    lateinit var mProgressBar: ProgressBar
 
     var auth:FirebaseAuth? = null
 
@@ -31,29 +34,40 @@ class LoginActivity : AppCompatActivity() {
         mLoginButton = findViewById(R.id.btn_login)
         mGoogleButton = findViewById(R.id.btn_google)
         mFacebookButton = findViewById(R.id.btn_facebook)
+        mProgressBar = findViewById(R.id.pb_login)
+
 
         auth = FirebaseAuth.getInstance()
 
-        mLoginButton.setOnClickListener{ signInAndSignUp(mEmailEditText.text.toString(), mPasswordEditText.text.toString())}
+        mLoginButton.setOnClickListener{
+
+            signInAndSignUp(mEmailEditText.text.toString(), mPasswordEditText.text.toString())
+
+        }
 
     }
 
     private fun signInAndSignUp(email:String, password:String) {
+        mProgressBar.visibility = View.VISIBLE
         auth?.createUserWithEmailAndPassword(email, password)?.addOnCompleteListener{
+
             try {
                 when {
                     it.isSuccessful -> startMainActivity()
-                    it.exception!!.message!!.isEmpty() -> signIn(email, password)
+                    it.exception!!.message!!.isNotEmpty() -> signIn(email, password)
                     else -> throw Exception(it.exception!!.message)
                 }
             } catch(e:Exception) {
                 Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
             }
+            mProgressBar.visibility = View.GONE
         }
     }
 
     private fun signIn(email: String, password: String) {
+        mProgressBar.visibility = View.VISIBLE
         auth?.signInWithEmailAndPassword(email, password)?.addOnCompleteListener{
+
             try {
                 when {
                     it.isSuccessful -> startMainActivity()
@@ -62,7 +76,9 @@ class LoginActivity : AppCompatActivity() {
             } catch(e:Exception) {
                 Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
             }
+            mProgressBar.visibility = View.GONE
         }
+
     }
 
     private fun startMainActivity() {
