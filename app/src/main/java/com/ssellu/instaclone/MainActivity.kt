a019b4c3e17527dca4080367f9e3bbc2a473bad0
Ssellu <ssellu.lee@gmail.com>
2021-03-03 10:25:03 +0900

    Add main Fragments

--- app/src/main/java/com/ssellu/instaclone/MainActivity.kt
+++ app/src/main/java/com/ssellu/instaclone/MainActivity.kt
@@ -2,10 +2,60 @@ package com.ssellu.instaclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.ssellu.instaclone.navigation.AlarmFragment
+import com.ssellu.instaclone.navigation.DetailViewFragment
+import com.ssellu.instaclone.navigation.GridFragment
+import com.ssellu.instaclone.navigation.UserFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+
+    lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigationView = findViewById(R.id.nav_bottom)
+        bottomNavigationView.setOnNavigationItemSelectedListener(this)
+
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.action_home ->{
+                val fragment = DetailViewFragment()
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.view_main, fragment)
+                    .commit()
+                return true
+            }
+            R.id.action_account ->{
+                val fragment = UserFragment()
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.view_main, fragment)
+                    .commit()
+                return true
+            }
+            R.id.action_favorite_alarm ->{
+                val fragment = AlarmFragment()
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.view_main, fragment)
+                    .commit()
+                return true
+            }
+            R.id.action_add_photo ->{
+                return true
+            }
+            R.id.action_search ->{
+                val fragment = GridFragment()
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.view_main, fragment)
+                    .commit()
+                return true
+            }
+        } // when
+        return false
     }
 }
---
+++ app/src/main/java/com/ssellu/instaclone/navigation/DetailViewFragment.kt
@@ -0,0 +1,19 @@
+package com.ssellu.instaclone.navigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.ssellu.instaclone.R
+
+class DetailViewFragment :Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = LayoutInflater.from(activity).inflate(R.layout.fragment_detail, container,false)
+        return view
+    }
+}
