7cbaf93970d808e264b7ac3a356f753b9d0710d8
Ssellu <ssellu.lee@gmail.com>
2021-03-12 23:20:38 +0900

    Refactor uploading photo

--- app/src/main/java/com/ssellu/instaclone/navigation/AddPhotoActivity.kt
+++ app/src/main/java/com/ssellu/instaclone/navigation/AddPhotoActivity.kt
@@ -5,12 +5,18 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.UploadTask
 import com.ssellu.instaclone.R
+import com.ssellu.instaclone.navigation.model.ContentDto
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -25,9 +31,10 @@ class AddPhotoActivity : AppCompatActivity() {
     lateinit var mImageView: ImageView
     lateinit var mEditText: EditText
 
-
-    var storage : FirebaseStorage? = null
-    var photoUri : Uri? = null
+    var auth: FirebaseAuth? = null
+    var firestore: FirebaseFirestore? = null
+    var storage: FirebaseStorage? = null
+    var photoUri: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,15 +45,18 @@ class AddPhotoActivity : AppCompatActivity() {
         mEditText = findViewById(R.id.et_description)
 
 
-        // Initiate Storage
+        // Initiate Firebase storage, auth, firestore
         storage = FirebaseStorage.getInstance()
+        auth = FirebaseAuth.getInstance()
+        firestore = FirebaseFirestore.getInstance()
+
 
         // Open Album
         val photoPickerIntent = Intent(Intent.ACTION_PICK)
         photoPickerIntent.type = "image/*"
         startActivityForResult(photoPickerIntent, PICK_IMAGE_FROM_ALBUM)
 
-        mButton.setOnClickListener{
+        mButton.setOnClickListener {
             contentUpload()
         }
 
@@ -57,19 +67,37 @@ class AddPhotoActivity : AppCompatActivity() {
         val imageFileName = "IMAGE_$timeStamp.png"
         val storageRef = storage?.reference?.child(IMAGE_PATH)?.child(imageFileName)
 
-        storageRef?.putFile(photoUri!!)?.addOnSuccessListener {
+        // https://firebase.google.com/docs/storage/android/upload-files?hl=ko#kotlin+ktx
+        storageRef?.putFile(photoUri!!)?.continueWithTask { task: Task<UploadTask.TaskSnapshot> ->
+            if (!task.isSuccessful) {
+                task.exception?.let {
+                    throw it
+                }
+            }
+            storageRef.downloadUrl
+        }?.addOnSuccessListener {  uri ->
             Toast.makeText(this, getString(R.string.upload_success), Toast.LENGTH_LONG).show()
+            val contentDto = ContentDto()
+            contentDto.imageUrl = uri.toString()
+            contentDto.uid = auth?.currentUser?.uid
+            contentDto.userId = auth?.currentUser?.email
+            contentDto.explain = mEditText.text.toString()
+            contentDto.timestamp = System.currentTimeMillis()
+            firestore?.collection("images")?.document()?.set(contentDto)
+            setResult(Activity.RESULT_OK)
+            finish()
+        }?.addOnFailureListener{
+            Toast.makeText(this, getString(R.string.upload_fail), Toast.LENGTH_LONG).show()
         }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode == PICK_IMAGE_FROM_ALBUM){
-            if(resultCode == Activity.RESULT_OK){
+        if (requestCode == PICK_IMAGE_FROM_ALBUM) {
+            if (resultCode == Activity.RESULT_OK) {
                 photoUri = data?.data
                 mImageView.setImageURI(photoUri)
-            }
-            else{
+            } else {
                 finish()
             }
         }
---
+++ app/src/main/java/com/ssellu/instaclone/navigation/model/ContentDto.kt
@@ -0,0 +1,18 @@
+package com.ssellu.instaclone.navigation.model
+
+data class ContentDto(
+    var explain: String? = "",
+    var imageUrl: String? = "",
+    var uid: String? = "",
+    var userId: String? = "",
+    var timestamp: Long? = null,
+    var favoriteCount: Int = 0,
+    var favorites: Map<String, Boolean> = HashMap()
+) {
+    data class Comment(
+        var uid: String? = "",
+        var userId: String? = "",
+        var comment: String? = "",
+        var timestamp: Long? = null
+    )
+}
